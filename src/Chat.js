b433d173e60fb45c6b37e41c76efe55039c65115
Syd7 <sayadi.hz@gmail.com>
2017-10-20 12:50:53 +0100

    implement socket io rooms

--- src/Chat.js
+++ src/Chat.js
@@ -1,78 +1,117 @@
 import $ from 'jquery';
 const React = require("react");
+var uniqid = require('uniqid');
 
 class Chat extends React.Component {
 
   constructor(props) {
     super(props);
-    const SERVER_ADDRESS = 'http://127.0.0.1:3001' ;
+    const SERVER_ADDRESS = 'http://127.0.0.1:3001';
+    this.socket = window.io(SERVER_ADDRESS);
     this.state = {
-      messages : {},
-      socket: window.io(SERVER_ADDRESS),
-      user : "unnamed",
-      usernames : [],
-      chatboxes : [],
+      messages: {},
+      user: "unnamed",
+      usernames: [],
+      chatboxes: []
     };
+  }
+
+  checkboxExists(userObj, checkboxes) {
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].id === userObj.id)
+        return true
+    }
+    return false
   }
 
   componentDidMount() {
 
+    var self = this;
 
-    var self = this ;
+    this.socket.on("usernames", function(msg) {
+      self.setState({usernames: msg});
+    });
 
-    //Initialize listeners for chat server
-    this.state.socket.on("receive-private" , function (msg) {
-      if (typeof self.state.messages[msg.user] === 'undefined') {
-        if(msg.user == self.state.user){
-            self.state.messages[msg.to].push(msg);
-            self.forceUpdate();
-            return null;
-        }
-        else {
-          self.state.messages[msg.user] = [];
-        }
+    this.socket.on('message', function(data) {
+
+      let newMessages = {
+        ...self.state.messages
       }
-      self.state.messages[msg.user].push(msg);
-      if ( $.inArray(msg.user, self.state.chatboxes) == -1 ){
-        self.state.chatboxes.push(msg.user);
+      let newChatboxes = [...self.state.chatboxes]
+
+      if (newMessages[data.from.name] === undefined) {
+        newMessages[data.from.name] = []
       }
-      self.forceUpdate();
+
+      if (data.self) {
+        if (newMessages[data.to.name] === undefined) {
+          newMessages[data.to.name] = []
+        }
+        newMessages[data.to.name].push({msg: data.message, from: data.from})
+      } else {
+        newMessages[data.from.name].push({msg: data.message, from: data.from})
+
+        if (!self.checkboxExists(data.from, self.state.chatboxes) && (data.self !== true)) {
+          newChatboxes.push({name: data.from.name, id: data.from.id});
+        }
+
+      }
+
+      self.setState({messages: newMessages, chatboxes: newChatboxes})
+
     });
-    this.state.socket.on("usernames" , function (msg) {
-      self.setState({ usernames : msg });
-    });
+
   }
 
-
   //pick a username
-  pickUser(){
-    var user = document.getElementById('username').value;
-    this.setState({"user" : user });
-    this.state.socket.emit("new user" , user);
+  pickUser() {
+    const userName = document.getElementById('username').value;
+    const userObj = {
+      name: userName,
+      id: uniqid()
+    }
+
+    this.socket.emit("new user", userObj);
+
+    this.setState({user: userObj});
+
   }
 
   //add a chatbox for user u
-  chatWith(u){
-    if ( $.inArray(u, this.state.chatboxes) == -1 ){
+  chatWith(u) {
+
+    if (!this.checkboxExists(u, this.state.chatboxes)) {
+      this.setState({cb: this.state.chatboxes.push(u)});
+    }
+
+    if (typeof this.state.messages[u] === 'undefined') {
+      let newMessages = {...this.state.messages} ;
+      newMessages[u] = []
       this.setState({
-        cb : this.state.chatboxes.push(u)
-      });
+        messages : newMessages
+      })
+
     }
-    if(typeof this.state.messages[u] == 'undefined'){
-      this.state.messages[u] = [];
-    }
+
   }
 
   //send private message to user u
-  sendTo(u){
+  sendTo(u) {
+
     var msg = document.getElementById(u).value;
     document.getElementById(u).value = "";
-    this.state.socket.emit("private-message" , {from : this.state.user , to : u , body : msg });
+
+    this.socket.emit('send', {
+      from: this.state.user,
+      to: u,
+      message: msg
+    });
+
   }
 
   //minimizing a chatbox
-  minmizeChatBox(event){
-    if(event.target.parentElement.nextSibling.style.display == "block")
+  minmizeChatBox(event) {
+    if (event.target.parentElement.nextSibling.style.display === "block")
       event.target.parentElement.nextSibling.style.display = "none";
     else {
       event.target.parentElement.nextSibling.style.display = "block";
@@ -80,61 +119,108 @@ class Chat extends React.Component {
   }
 
   //closing a chatbox
-  closeChatBox(cb){
-    console.log("closing chatbox "+cb);
+  closeChatBox(cb) {
+
     const newChatboxes = this.state.chatboxes;
-    newChatboxes.splice($.inArray(cb, newChatboxes),1);
-    this.setState({
-      chatboxes : newChatboxes
-    });
+    newChatboxes.splice($.inArray(cb, newChatboxes), 1);
+    this.setState({chatboxes: newChatboxes});
   }
 
+  renderConnected() {
+    if (this.state.user === "unnamed") {
+      return (
+        <div>
+          <div>
+            <div className="form-group">
+              <label >Name:</label>
+              <input type="text" id="username" className="form-control"/>
+            </div>
+
+          </div>
+          <div className="btn btn-primary" onClick={this.pickUser.bind(this)}>
+            Connect
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h4>
+            Connected as {this.state.user.name} !
+          </h4>
+        </div>
+      )
+    }
+  }
   render() {
 
     var self = this;
 
     //Render connected users
-    let i = 0 ;
-    var usernames = this.state.usernames.map(function (users) {
-        i++ ;
+    let i = 0;
+    var usernames = this.state.usernames.map(function(user) {
+      i++;
+      if (user.id !== self.state.user.id) {
         return (
           <div key={i} className="sidebar-name">
             <a>
-              <img width="30" height="30" src="https://upload.wikimedia.org/wikipedia/commons/d/d3/User_Circle.png" />
-           <span onClick={() => self.chatWith(users)}> {users} </span>
-          </a>
+              <img width="30" height="30" alt="profile_image" src="https://upload.wikimedia.org/wikipedia/commons/d/d3/User_Circle.png"/>
+              <span onClick={() => self.chatWith(user)}>
+                {user.name}
+              </span>
+            </a>
           </div>
         )
+      }
+      else {
+        return null
+      }
     });
-    let j = 1 ;
+    let j = 1;
 
     //Render Chatboxes
-    var chatboxes = self.state.chatboxes.map(function (cb) {
+    var chatboxes = self.state.chatboxes.map(function(cb) {
       let divStyle = {
-        right : j*220
+        right: j*220
       };
       j++;
       let privatmessages;
-      if (typeof self.state.messages[cb] !== 'undefined'){
-        privatmessages = self.state.messages[cb].map(function (msg , index) {
-        return <div key={index}> {msg.user} : {msg.body} <br/> </div>;
-      });
-      }
-      else{
+      if (typeof self.state.messages[cb.name] !== 'undefined') {
+        privatmessages = self.state.messages[cb.name].map(function(msg, index) {
+          return <div key={index}>
+            {msg.from.name}
+            : {msg.msg}
+            <br/>
+          </div>;
+        });
+      } else {
         privatmessages = null;
       }
-      const chatboxId = "chatbox-"+cb ;
+
+      const chatboxId = "chatbox-" + cb;
       return (
-        <div className="chatbox" style={divStyle} key={j}>
-          <div className="head">Chat with {cb}
-                <span onClick={() => self.closeChatBox(cb)} style={{marginLeft : "10px"}} className="popup-head-right"> x </span>
-                <span onClick={self.minmizeChatBox} className="popup-head-right"> - </span>
+        <div key={chatboxId} className="chatbox" style={divStyle}>
+          <div className="head">Chat with {cb.name}
+            <span onClick={() => self.closeChatBox(cb)} style={{
+              marginLeft: "10px"
+            }} className="popup-head-right">
+              X
+            </span>
+            <span onClick={self.minmizeChatBox} className="popup-head-right">
+              -
+            </span>
           </div>
-          <div className="content" style={{display:"block"}}>
-            <div> {privatmessages}</div>
+          <div className="content" style={{
+            display: "block"
+          }}>
+            <div>
+              {privatmessages}
+            </div>
             <div className="chat-on">
               <input id={cb} type="text"/>
-              <button onClick={() => self.sendTo(cb)}> Send </button>
+              <button onClick={() => self.sendTo(cb)}>
+                Send
+              </button>
             </div>
           </div>
         </div>
@@ -142,28 +228,20 @@ class Chat extends React.Component {
       );
     })
 
-
     return (
       <div>
         <div className="side-bar">
-          <div>
-            <div className="form-group">
-                <label for="usr">Name:</label>
-                <input type="text" id="username" className="form-control" />
-            </div>
-
-          </div>
-          <div className="btn btn-primary" onClick={this.pickUser.bind(this)} >
-            choose username
-          </div>
+          {this.renderConnected()}
           <div className="connected-users">
 
-            <label for="usr">Connected Users ( {this.state.usernames.length} ) :  </label>
+            <label >Connected Users ( { (this.state.usernames.length === 0) ? 0 : this.state.usernames.length - 1  }
+              ) :
+            </label>
             {usernames}
           </div>
         </div>
         <div>
-            {chatboxes}
+          {chatboxes}
         </div>
       </div>
     );
--- src/server.js
+++ src/server.js
@@ -2,62 +2,50 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+
 var users = {};
 var nicknames = [];
-io.sockets.on("connection" , function(socket){
-  console.log("somone connected");
-  updateNicknames();
-  socket.on('new user' , function(data , callback){
 
+function updateNicknames() {
+  io.sockets.emit('usernames', nicknames);
+}
 
-        socket.user = data ;
-        users[socket.user] = socket;
-        nicknames.push(socket.user);
-        console.log(nicknames);
-        updateNicknames();
+io.sockets.on("connection", function(socket) {
+
+  socket.on('new user', function(data, callback) {
+
+    socket.user = data;
+    users[socket.user] = socket;
+    nicknames.push(socket.user);
+    socket.join(data.id);
+    console.log('joining room  :  ', data);
+    updateNicknames();
 
   })
 
-  function updateNicknames() {
-    io.sockets.emit('usernames' , nicknames);
-  }
-  socket.on("private-message" , function(msg){
-    console.log("sending private msg to "+msg.to );
-    var socket = users[msg.to];
-    socket.emit("receive-private" ,{ user : msg.from , to : msg.to ,  body : msg.body});
-    users[msg.from].emit("receive-private" ,{ user : msg.from ,to : msg.to , body : msg.body});
+
+  socket.on('send', function(data) {
+    console.log('sending message  :   ', data);
+
+    data["self"] = false
+    io.sockets.in(data.to.id).emit('message', data);
+
+    data["self"] = true
+    io.sockets.in(data.from.id).emit('message', data);
   });
-  socket.on("new-message" , function(msg){
-    console.log(msg.body.substr(0,1));
-    if (msg.body.substr(0,1) == 'w') {
-      console.log("whispering");
-      var msgn;
-      msgn = msg.body.substr(1);
-      var ind = msgn.indexOf(' ');
-      console.log(ind);
-      if (ind !== -1){
-        var name = msgn.substring(0, ind);
-        var msgn = msgn.substring(ind + 1);
-        console.log(name);
-        if (name in nicknames) {
-            users[name].emit('whisper',{user : msg.name , body : msgn});
-        }
-      }
-    }
-    io.emit("receive-msg" , msg);
-  })
-  socket.on("test", function () {
-    console.log("mounted");
-  });
-  socket.on('disconnect' , function (data) {
+
+
+
+  socket.on('disconnect', function(data) {
     if (!socket.user) return null;
-    console.log("somone disconnected");
-    nicknames.splice(nicknames.indexOf(socket.user) , 1);
+    nicknames.splice(nicknames.indexOf(socket.user), 1);
     updateNicknames();
   })
+
 });
 
 
-http.listen('3001' , function() {
+http.listen('3001', function() {
   console.log("We're connected");
 });
